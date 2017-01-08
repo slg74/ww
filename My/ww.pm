7db945a621953246066846d72870253f41fa0701
scott <scott.gillespie@r1soft.com>
2017-01-08 00:28:27 -0600

    cleanup

--- My/ww.pm
+++ My/ww.pm
@@ -8,18 +8,23 @@ use Exporter qw(import);
 
 our @EXPORT_OK = qw(get_world_writable unset_world_writable);
 
+# make sure umask doesn't bite us
 $File::chmod::UMASK = 0;
 
 sub get_world_writable {
     my $dir = shift;
     my @world_writable;
-    open my $in, '-|', "find $dir -perm -o=w -exec ls -l {} \\;", or die "cannot run: $!";
+
+    open my $in, '-|', "find $dir -perm -2 ! -type l -ls", or die; 
     while (<$in>) {
-        my @line = split;
-        my $filename = $line[8];
-        push @world_writable, $filename if $filename;
+        # get 10th column of find output       
+        my @line     = split;
+        my $file = $line[10];
+
+        push @world_writable, $file if $file;
     }
     close $in;
+
     return @world_writable
 }
 
@@ -27,6 +32,9 @@ sub unset_world_writable {
     my $file = shift;
     my $st   = stat($file); 
     my $mode;
+
+    # if we get a stat(), and the current global mode with 
+    # the negative permission set, ~S_IWOTH. 
     if ($st) {
         $mode = $st->mode;
         chmod($mode & ~S_IWOTH, $file);
