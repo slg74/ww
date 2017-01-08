4d5a4d6d20e77ff6ea2090e1dad0d97676739b8b
scott <scott.gillespie@r1soft.com>
2017-01-07 23:09:09 -0600

    update

--- My/ww.pm
+++ My/ww.pm
@@ -5,26 +5,26 @@ use warnings;
 use File::chmod;
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(get_world_writable_files unset_world_writable);
+our @EXPORT_OK = qw(get_world_writable unset_world_writable);
 
 $File::chmod::UMASK = 0;
 
-sub get_world_writable_files {
+sub get_world_writable {
     my $dir = shift;
-    my @world_writable_files;
+    my @world_writable;
     open my $in, '-|', "find $dir -perm -o=w -exec ls -l {} \\;", or die "cannot run: $!";
     while (<$in>) {
         my @line = split;
         my $filename = $line[8];
-        push @world_writable_files, $filename if $filename;
+        push @world_writable, $filename if $filename;
     }
     close $in;
-    return @world_writable_files;
+    return @world_writable
 }
 
 sub unset_world_writable {
     my $file = shift;
-    chmod("o-w", $file); 
+    chmod("o-w", $file), if $file; 
 }
 
 1;
