c6a5736d30a9fdc82800eb71fbfbbae058d22c3e
scott <scott.gillespie@r1soft.com>
2017-01-07 22:01:28 -0600

    convert tabs to 4 spaces. this isn't golang lol.

--- My/ww.pm
+++ My/ww.pm
@@ -10,21 +10,21 @@ our @EXPORT_OK = qw(get_wwritable_files unset_wwritable);
 $File::chmod::UMASK = 0;
 
 sub get_wwritable_files {
-	my $dir = shift;
-	my @ww_filenames;
-	open my $in, '-|', "find $dir -perm -o=w -exec ls -l {} \\;", or die "cannot run: $!";
-	while (<$in>) {
-		my @line = split;
-		my $f = $line[8];
-		push @ww_filenames, $f if $f;
-	}
-	close $in;
-	return @ww_filenames;
+    my $dir = shift;
+    my @ww_filenames;
+    open my $in, '-|', "find $dir -perm -o=w -exec ls -l {} \\;", or die "cannot run: $!";
+    while (<$in>) {
+        my @line = split;
+        my $f = $line[8];
+        push @ww_filenames, $f if $f;
+    }
+    close $in;
+    return @ww_filenames;
 }
 
 sub unset_wwritable {
-	my $f = shift;
-	chmod("o-w", $f); 
+    my $f = shift;
+    chmod("o-w", $f); 
 }
 
 1;
